bf396ad75c397994ad0d5db324d1e3ae77418f51
André Neves <andreneves3@gmail.com>
2022-12-24 21:02:31 -0300

    test(get notifications usecase): test all notifications retrieval

--- src/domains/notifications/domain/usecases/get-notifications/get-notifications.usecase.spec.ts
+++ src/domains/notifications/domain/usecases/get-notifications/get-notifications.usecase.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoreModule } from '../../../../../core/core.module';
 import { InMemoryNotificationsRepository } from '../../../infra/repositories/in-memory/in-memory-notifications-repository';
+import { NotificationFactory } from '../../factories/notification-factory/notification-factory';
 import { NotificationsRepository } from '../../repositories/notifications-repository.interface';
 import { GetNotificationsUsecase } from './get-notifications.usecase';
 
 describe('GetNotificationsUsecase', () => {
-  let provider: GetNotificationsUsecase;
+  let usecase: GetNotificationsUsecase;
+  let repository: NotificationsRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -19,10 +21,34 @@ describe('GetNotificationsUsecase', () => {
       ],
     }).compile();
 
-    provider = module.get<GetNotificationsUsecase>(GetNotificationsUsecase);
+    usecase = module.get<GetNotificationsUsecase>(GetNotificationsUsecase);
+    repository = module.get<NotificationsRepository>(NotificationsRepository);
   });
 
   it('should be defined', () => {
-    expect(provider).toBeDefined();
+    expect(usecase).toBeDefined();
+  });
+
+  it('should be able to get notifications', async () => {
+    const createdNotifications = [
+      NotificationFactory.makeMock({
+        recipientId: 'example-recipient-id',
+      }),
+      NotificationFactory.makeMock({
+        recipientId: 'example-recipient-id',
+      }),
+      NotificationFactory.makeMock({
+        recipientId: 'new-recipient-id',
+      }),
+    ];
+
+    const promises = createdNotifications.map((notification) =>
+      repository.create(notification),
+    );
+    await Promise.all(promises);
+
+    const { notifications } = await usecase.execute();
+
+    expect(notifications).toHaveLength(3);
   });
 });
