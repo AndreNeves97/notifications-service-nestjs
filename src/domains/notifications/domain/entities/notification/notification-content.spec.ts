475faaa8c2ef91d5d056e048f28f3c3a6295e496
André Neves <andreneves3@gmail.com>
2022-12-24 23:16:23 -0300

    test(notification content): strict check the error returned on content length issues

--- src/domains/notifications/domain/entities/notification/notification-content.spec.ts
+++ src/domains/notifications/domain/entities/notification/notification-content.spec.ts
@@ -1,20 +1,21 @@
+import { ContentLengthError } from '../../errors/content-length-error';
 import { NotificationContent } from './notification-content';
 
 describe('NotificationContent', () => {
   it('should be able to create a notification content', () => {
     const content = new NotificationContent(
-      'Você recebeu uma solicitação de amizade',
+      'You have received a friend request',
     );
 
     expect(content).toBeTruthy();
   });
 
   it('should not be able to create a notification content with less than 5 characters', () => {
-    expect(() => new NotificationContent('Você')).toThrow();
+    expect(() => new NotificationContent('Você')).toThrow(ContentLengthError);
   });
 
   it('should not be able to create a notification content with more than 240 characters', () => {
     const content = 'a'.repeat(241);
-    expect(() => new NotificationContent(content)).toThrow();
+    expect(() => new NotificationContent(content)).toThrow(ContentLengthError);
   });
 });
